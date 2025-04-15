a84ca75ce2f33ef78cb29c9059863453f715562e
Eric Schmidt <eric0802@web.de>
2025-04-15 14:04:35 +0200

    Initial commit

---
+++ src/main/java/org/example/javangersspring/JavAngersSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.javangersspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAngersSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavAngersSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javangersspring/JavAngersSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javangersspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavAngersSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
